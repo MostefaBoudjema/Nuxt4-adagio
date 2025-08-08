ee848c263a84fdd2bb7b056d680b0fed9ab8c6cd
mostfa <myreal17@gmail.com>
2025-08-08 14:27:20 +0100

    update index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,13 +8,11 @@
       <appointments />
       <departments />
       <doctors />
-       <faq />
-       <testimonials/>
-        <gallery />
+      <faq />
+      <testimonials />
+      <gallery />
       <contact />
     </main>
-    <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
-        class="bi bi-arrow-up-short"></i></a>
 
   </div>
 </template>
